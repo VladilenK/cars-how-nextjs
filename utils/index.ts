2e4c41ff3fcd4b95c3be1ca0b60ff8f801cdc6af
Vladilen Karassev <Vladilen@Karassev.com>
2023-06-14 17:00:21 -0500

    Revert "upd 6/14"
    This reverts commit ad9084caeba107232937dffbb8effd2d3e28f019.

--- utils/index.ts
+++ utils/index.ts
@@ -2,8 +2,8 @@ import { CarProps, FilterProps } from "@/types";
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
   const basePricePerDay = 50; // Base rental price per day in dollars
-  const mileageFactor = 0.2; // Additional rate per mile driven
-  const ageFactor = 0.1; // Additional rate per year of vehicle age
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
 
   // Calculate additional rate based on mileage and age
   const mileageRate = city_mpg * mileageFactor;
